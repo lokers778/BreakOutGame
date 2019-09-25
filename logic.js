6c0d3866cd3d2edd474f85a0e16b2b2fc72291d7
Łukasz <lokers778@gmail.com>
2019-09-25 23:44:57 +0200

    added moving ball

--- logic.js
+++ logic.js
@@ -26,17 +26,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         move() {
+
             document.addEventListener("keydown", (event) => {
+
                 if (event.keyCode === 37 || event.keyCode === 65) {
+                    context.clearRect(this.position.x+5, this.position.y-0.5, this.width, this.height) //
                     if (this.position.x > 0) {
                         this.position.x -= this.speed;
                     }
                 } else if (event.keyCode === 39 || event.keyCode === 68) {
+                    context.clearRect(this.position.x-5, this.position.y-0.5, this.width, this.height) //
                     if (this.position.x < gameWidth - this.width) {
                         this.position.x += this.speed
                     }
                 }
-                context.clearRect(0, 0, gameWidth, gameHeight) //
+
                 this.draw(context)
 
             });
@@ -49,19 +53,31 @@ document.addEventListener("DOMContentLoaded", function () {
     class BallCanvas {
         constructor() {
             this.ball = document.querySelector("img");
+            this.speed={x:5,y:5};
+            this.position={x:10,y:10}
         }
 
         draw(context) {
-            context.drawImage(this.ball, 10, 10, 20, 20)
+
+            context.drawImage(this.ball, this.position.x, this.position.y, 20, 20)
+        }
+        update(){
+            setInterval(()=>{
+                context.clearRect(this.position.x, this.position.y, 20, 20) //
+                this.position.x +=this.speed.x;
+                this.position.y +=this.speed.y;
+                ball.draw(context);
+            },100)
+
         }
     }
-
 
     let paddle = new Paddle(gameWidth, gameHeight);
     let ball = new BallCanvas();
 
     paddle.draw(context);
     ball.draw(context);
+    ball.update()
     paddle.move();
 
 });
