ca85ee848a769776281f3d3ad9a1a86768443d4e
Łukasz <lokers778@gmail.com>
2019-10-14 23:37:16 +0200

    control panel added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #Break Out Game
 Retro game , made with vanilla js, canvas es6. My attempt to become game dev ->project rough on edges but made with passion.
-Contains function responsible for creating lvl, still must add player control panel.
+Contains function responsible for creating lvl.
 ##App demo
 https://lokers778.github.io/BreakOutGame/
+
+Use arrow or a/d to move.
--- logic.js
+++ logic.js
@@ -1,5 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
 
+const level_1 = [
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+];
+const level_2 = [
+    [0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 0],
+];
+const level_3 = [
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+const level_4 = [
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 1],
+]
+
+let setGame=(level)=> {
     let canvas = document.querySelector("canvas");
     let context = canvas.getContext("2d");
     let gameWidth = window.innerWidth;
@@ -30,12 +66,12 @@ document.addEventListener("DOMContentLoaded", function () {
             document.addEventListener("keydown", (event) => {
 
                 if (event.keyCode === 37 || event.keyCode === 65) {
-                    context.clearRect(this.position.x+5, this.position.y-0.5, this.width, this.height) //
+                    context.clearRect(this.position.x + 5, this.position.y - 0.5, this.width, this.height) //
                     if (this.position.x > 0) {
                         this.position.x -= this.speed;
                     }
                 } else if (event.keyCode === 39 || event.keyCode === 68) {
-                    context.clearRect(this.position.x-5, this.position.y-0.5, this.width, this.height) //
+                    context.clearRect(this.position.x - 5, this.position.y - 0.5, this.width, this.height) //
                     if (this.position.x < gameWidth - this.width) {
                         this.position.x += this.speed
                     }
@@ -53,49 +89,51 @@ document.addEventListener("DOMContentLoaded", function () {
     class BallCanvas {
         constructor() {
             this.ball = document.querySelector(".ball");
-            this.speed={x:10,y:-10};
-            this.position={x:gameWidth/2,y:gameHeight+paddle.height}
-            this.size=40;
+            this.speed = {x: 10, y: -10};
+            this.size = 50;
+            this.position = {x: gameWidth / 2, y: gameHeight  - 2*this.size }
         }
 
         draw(context) {
 
-            context.drawImage(this.ball, this.position.x, this.position.y, this.size, this.size)
+            context.drawImage(this.ball, this.position.x, this.position.y  , this.size, this.size)
         }
-        update(bricks){
-            setInterval(()=>{
+
+        update(bricks) {
+            setInterval(() => {
                 context.clearRect(this.position.x, this.position.y, this.size, this.size)
-                this.position.x +=this.speed.x;
-                this.position.y +=this.speed.y;
-                if(this.position.x+this.size>gameWidth||this.position.x<0){
-                    this.speed.x=-this.speed.x;
+                this.position.x += this.speed.x;
+                this.position.y += this.speed.y;
+                if (this.position.x + this.size > gameWidth || this.position.x < 0) {
+                    this.speed.x = -this.speed.x;
                 }
-                if(this.position.y+this.size<-10){
-                    this.speed.y=-this.speed.y;
+                if (this.position.y + this.size < -10) {
+                    this.speed.y = -this.speed.y;
                 }
-                if(this.position.y+this.size>gameHeight){
-                    console.log("you lost")
+                if (this.position.y + this.size > gameHeight) {
+                    alert("you lost :(")
+                    location.reload();
                 }
-                if(this.position.y+this.size>=paddle.position.y && this.position.x>paddle.position.x && this.position.x<paddle.position.x +paddle.width ){
-                    this.speed.y=-this.speed.y;
-                    this.position.y=paddle.position.y-this.size
+                if (this.position.y + this.size >= paddle.position.y && this.position.x > paddle.position.x && this.position.x < paddle.position.x + paddle.width) {
+                    this.speed.y = -this.speed.y;
+                    this.position.y = paddle.position.y - this.size
                 }
-                bricks.forEach((brick,index)=>{
-                    if(this.position.y +this.size >=brick.position.y && this.position.y<=brick.position.y + brick.height && this.position.x>= brick.position.x && this.position.x + this.size<=brick.position.x +brick.width){
-                        this.speed.y=-this.speed.y;
-                        bricks.splice(index,1)
+                bricks.forEach((brick, index) => {
+                    if (this.position.y + this.size >= brick.position.y && this.position.y <= brick.position.y + brick.height && this.position.x >= brick.position.x && this.position.x + this.size <= brick.position.x + brick.width) {
+                        this.speed.y = -this.speed.y;
+                        bricks.splice(index, 1)
                         context.clearRect(brick.position.x, brick.position.y, brick.width, brick.height)
-                        if(bricks.length===0){
-                            console.log("Wyyyyyygrana")
+                        if (bricks.length === 0) {
+                            alert("Wyyyyyygrana")
+                            location.reload();
                         }
                     }
 
                 })
 
 
-
                 ball.draw(context);
-            },50)
+            }, 50)
 
         }
     }
@@ -103,9 +141,9 @@ document.addEventListener("DOMContentLoaded", function () {
     class Brick {
         constructor(position) {
             this.brick = document.querySelector(".brick");
-            this.width = gameWidth/10;
-            this.height=gameHeight/20;
-            this.position=position
+            this.width = gameWidth / 10;
+            this.height = gameHeight / 20;
+            this.position = position
         }
 
         draw(context) {
@@ -117,65 +155,41 @@ document.addEventListener("DOMContentLoaded", function () {
     let ball = new BallCanvas();
 
 
-    const level_1=[
-        [0,1,0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0,1,0],
-        [0,1,0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0,1,0],
-        [0,1,0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0,1,0],
-    ];
-    const level_2=[
-        [0,1,1,0,0,1,1,0,0,1],
-        [1,0,0,1,1,0,0,1,1,0],
-        [0,1,1,0,0,1,1,0,0,1],
-        [1,0,0,1,1,0,0,1,1,0],
-        [0,1,1,0,0,1,1,0,0,1],
-        [1,0,0,1,1,0,0,1,1,0],
-    ];
-    const level_3=[
-        [0,1,0,1,0,1,0,1,0,1],
-        [1,1,1,1,1,1,1,1,1,1],
-        [1,1,1,1,1,1,1,1,1,1],
-        [1,1,1,1,1,1,1,1,1,1],
-        [1,1,1,1,1,1,1,1,1,1],
-    ];
-    const level_4=[
-        [1,0,1,0,0,1,0,1,0,1],
-        [1,0,1,0,0,1,0,1,0,1],
-        [1,0,1,1,1,1,0,1,0,1],
-        [1,0,1,1,1,1,0,1,0,1],
-        [1,0,1,1,1,1,0,1,0,1],
-        [1,0,1,1,1,1,0,1,0,1],
-        [1,0,1,1,1,1,0,1,0,1],
-        [1,0,0,1,1,0,0,1,0,1],
-        [1,0,0,1,1,0,0,1,1,1],
-        [1,0,0,1,1,0,0,1,1,1],
-    ]
 
-    let bricks =[];
-    const buildLevel=(level)=>{
-        bricks=[];
 
-        level.forEach((row,rowsItem)=>{
-            row.forEach((brick,bricksItem)=>{
-                if(brick === 1){
-                    let position={
-                        x:gameWidth/10* bricksItem,
-                        y: gameHeight/20 + gameHeight/20 * rowsItem
+    let bricks = [];
+    const buildLevel = (level) => {
+        bricks = [];
+
+        level.forEach((row, rowsItem) => {
+            row.forEach((brick, bricksItem) => {
+                if (brick === 1) {
+                    let position = {
+                        x: gameWidth / 10 * bricksItem,
+                        y: gameHeight / 20 + gameHeight / 20 * rowsItem
                     }
-                  bricks.push(new Brick(position))
+                    bricks.push(new Brick(position))
                 }
             })
         })
     }
 
-    buildLevel(level_4)
+    buildLevel(level)
     paddle.draw(context);
     ball.draw(context);
     ball.update(bricks);
     paddle.move();
-    bricks.forEach((e)=>{
+    bricks.forEach((e) => {
         e.draw(context)
     })
-});
+}
+document.querySelectorAll("li").forEach((e)=>{
+    e.addEventListener("click",()=>{
+        console.log(e.dataset.lvl)
+        let array = [level_1,level_2,level_3,level_4]
+      setGame(array[e.dataset.lvl-1])
+        let canvas = document.querySelector("canvas");
+        canvas.style.display="block"
+        document.querySelector("#startingScreen").style.display="none"
+    })
+})
